58ab3e9985d5b127d613c9dde7177b4f33230c66
Jack Andersen <jackoalan@gmail.com>
2015-05-27 23:23:28 -1000

    OS X build working

--- main.c
+++ main.c
@@ -2,14 +2,19 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <math.h>
 
 #define VOTE_ALLOC_COUNT 1024
 #define BLOCK_SAMPLES 14
 
+#ifdef __linux__
 #define ALSA_PLAY 1
+#endif
+#if ALSA_PLAY
 #include <alsa/asoundlib.h>
 snd_pcm_t* ALSA_PCM;
+#endif
 FILE* WAVE_FILE_OUT = NULL;
 
 static int64_t ERROR_AVG = 0;
